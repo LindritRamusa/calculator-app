8ee072c0a6194923b1eb8b019653b6e9eb372ea5
LindritRamusa <lindrit.ramusa5@gmail.com>
2025-09-10 22:15:20 +0200

    feat: fixed overall margins and sizing of calculator and added funcionality of pressing a number and showing it in display

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 'strict'
+const hud = document.querySelector('.hud-content');
+const numberButtons = document.querySelectorAll('.number-btn');
+
+function appendNumberToDisplay(number){
+    hud.value += number;
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', ()=>{
+        appendNumberToDisplay(button.value);
+    })
+})
+
